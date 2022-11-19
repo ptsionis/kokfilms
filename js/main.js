3dce420086bba8a115c7c42534c46c3561512219
Panagiotis Tsionis <tsionispanagiotis@gmail.com>
2022-11-19 20:18:48 +0200

    temp

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
+"use strict";
+
 window.addEventListener('load', () => {
     document.getElementById("fullpage-wrapper").style.display = "block";
-    preloader.classList.add("preloader-finish");
+    document.getElementById("preloader").classList.add("preloader-finish");
     document.querySelector("#fp-nav").style.display = "block";
     setTimeout(() => {
         document.getElementById("preloader").style.display = 'none';
     }, 500)
-})
+});
