837628f4a418067db3439ded61f9e4c30ce8130a
Panagiotis Tsionis <tsionispanagiotis@gmail.com>
2022-11-21 01:09:14 +0200

    fixed problem with video

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 "use strict";
 
-const musicVideo1 = document.getElementById("music-video1");
-const musicVideo2 = document.getElementById("music-video2");
-const musicVideo3 = document.getElementById("music-video3");
+const musicVideoList = document.querySelectorAll(".music-video");
 
 window.addEventListener('load', () => {
     document.getElementById("fullpage-wrapper").style.display = "block";
@@ -10,13 +8,17 @@ window.addEventListener('load', () => {
     document.querySelector("#fp-nav").style.display = "block";
     setTimeout(() => {
         document.getElementById("preloader").style.display = 'none';
-    }, 500)
+    }, 500);
+
+    if (!musicVideoList.length==0) {
+        musicVideoList[0].play();
+    }
 });
 
-let musicVideoOrder = 1;
+let musicVideoOrder = 0;
 
-document.querySelector(".fp-arrow.fp-prev").addEventListener('click', prevMusicSlide());
-document.querySelector(".fp-arrow.fp-next").addEventListener('click', nextMusicSlide());
+document.querySelector(".fp-arrow.fp-prev").addEventListener('click', function() {prevMusicSlide()});
+document.querySelector(".fp-arrow.fp-next").addEventListener('click', function() {nextMusicSlide()});
 document.addEventListener("keydown", (e) => {
     if (e.keyCode == 37) {
         prevMusicSlide();
@@ -24,66 +26,34 @@ document.addEventListener("keydown", (e) => {
     else if (e.keyCode == 39) {
         nextMusicSlide();
     }
-})
+});
 
 function prevMusicSlide() {
-    musicVideo1.pause();
-    musicVideo2.pause();
-    musicVideo3.pause();
+    musicVideoList.forEach(element => {
+        element.pause();
+    });
 
     musicVideoOrder--;
-    switch (musicVideoOrder) {
-        case 1:
-            musicVideo1.play();
-            musicVideo2.pause();
-            musicVideo3.pause();
-            break;
-        case 2:
-            musicVideo1.pause();
-            musicVideo2.play();
-            musicVideo3.pause();
-            break;
-        case 3:
-            musicVideo1.pause();
-            musicVideo2.pause();
-            musicVideo3.play();
-            break;
-        default:
-            musicVideo1.pause();
-            musicVideo2.pause();
-            musicVideo3.play();
-            musicVideoOrder = 1;
-            break;
+    if (musicVideoOrder < 0) {
+        musicVideoList[musicVideoList.length-1].play();
+        musicVideoOrder = 2;
+    }
+    else {
+        musicVideoList[musicVideoOrder].play();
     }
 }
 
 function nextMusicSlide() {
-    musicVideo1.pause();
-    musicVideo2.pause();
-    musicVideo3.pause();
+    musicVideoList.forEach(element => {
+        element.pause();
+    });
 
     musicVideoOrder++;
-    switch (musicVideoOrder) {
-        case 1:
-            musicVideo1.play();
-            musicVideo2.pause();
-            musicVideo3.pause();
-            break;
-        case 2:
-            musicVideo1.pause();
-            musicVideo2.play();
-            musicVideo3.pause();
-            break;
-        case 3:
-            musicVideo1.pause();
-            musicVideo2.pause();
-            musicVideo3.play();
-            break;
-        default:
-            musicVideo1.play();
-            musicVideo2.pause();
-            musicVideo3.pause();
-            musicVideoOrder = 1;
-            break;
+    if (musicVideoOrder < 0) {
+        musicVideoList[0].play();
+        musicVideoOrder = 0;
+    }
+    else {
+        musicVideoList[musicVideoOrder].play();
     }
 }
